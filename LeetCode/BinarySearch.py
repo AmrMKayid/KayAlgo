b6e1c14f3253b31aa324e131ac20ed92b80c10e2
Amr M. Kayid <amrkayid2027@gmail.com>
2018-06-19 09:11:24 +0200

    Define BS class

--- LeetCode/BinarySearch.py
+++ LeetCode/BinarySearch.py
@@ -1,49 +1,53 @@
-def BinarySearch_Recursive(arr, left, right, value):
-	''' implemntation of Binary Search Algorithm'''
-	if right >= left:
-		mid = left + (right - left) // 2
-		if (value == arr[mid]):
-			return "Value {} found @ index {}".format(value, mid)
-		elif(value < arr[mid]):
-			return BinarySearch_Recursive(arr, left, mid - 1, value)
-		elif(value > arr[mid]):
-			return BinarySearch_Recursive(arr, mid + 1, right, value)
-	else:
+class BinarySearch():
+	def Recursive(self, arr, left, right, value):
+		''' implemntation of Recursive Binary Search Algorithm'''
+		if right >= left:
+			mid = left + (right - left) // 2
+			if (value == arr[mid]):
+				return "Value {} found @ index {}".format(value, mid)
+			elif(value < arr[mid]):
+				return self.Recursive(arr, left, mid - 1, value)
+			elif(value > arr[mid]):
+				return self.Recursive(arr, mid + 1, right, value)
+		else:
+			return "Value does not exist on this array"
+
+
+	def Iterative(self, arr, value):
+		''' implemntation of Iterative Binary Search Algorithm'''
+		left = 0
+		right = len(arr) - 1
+		while right >= left:
+		    mid = (right + left)//2
+		    if(arr[mid] == value):
+		        return "Value {} found @ index {}".format(value, mid)
+		    elif(arr[mid] > value):
+		        right = mid - 1
+		    elif(arr[mid] < value):
+		        left = mid + 1
 		return "Value does not exist on this array"
 
-
-def BinarySearch_Iterative(arr, value):
-	left = 0
-	right = len(arr) - 1
-	while right >= left:
-	    mid = (right + left)//2
-	    if(arr[mid] == value):
-	        return "Value {} found @ index {}".format(value, mid)
-	    elif(arr[mid] > value):
-	        right = mid - 1
-	    elif(arr[mid] < value):
-	        left = mid + 1
-	return "Value does not exist on this array"
-
 def main():
+	bs = BinarySearch()
 	arr = [0, 1, 2, 3, 4, 5, 6, 7]
+
 	print("BinarySearch Recursive")
-	print(BinarySearch_Recursive(arr, 0, len(arr) - 1, -1))
-	print(BinarySearch_Recursive(arr, 0, len(arr) - 1, 6))
-	print(BinarySearch_Recursive(arr, 0, len(arr) - 1, 3))
-	print(BinarySearch_Recursive(arr, 0, len(arr) - 1, 0))
-	print(BinarySearch_Recursive(arr, 0, len(arr) - 1, 5))
-	print(BinarySearch_Recursive(arr, 0, len(arr) - 1, 7))
-	print(BinarySearch_Recursive(arr, 0, len(arr) - 1, 8))
+	print(bs.Recursive(arr, 0, len(arr) - 1, -1))
+	print(bs.Recursive(arr, 0, len(arr) - 1, 6))
+	print(bs.Recursive(arr, 0, len(arr) - 1, 3))
+	print(bs.Recursive(arr, 0, len(arr) - 1, 0))
+	print(bs.Recursive(arr, 0, len(arr) - 1, 5))
+	print(bs.Recursive(arr, 0, len(arr) - 1, 7))
+	print(bs.Recursive(arr, 0, len(arr) - 1, 8))
 
 	print("\nBinarySearch Iterative")
-	print(BinarySearch_Iterative(arr, -1))
-	print(BinarySearch_Iterative(arr, 6))
-	print(BinarySearch_Iterative(arr, 3))
-	print(BinarySearch_Iterative(arr, 0))
-	print(BinarySearch_Iterative(arr, 5))
-	print(BinarySearch_Iterative(arr, 7))
-	print(BinarySearch_Iterative(arr, 8))
+	print(bs.Iterative(arr, -1))
+	print(bs.Iterative(arr, 6))
+	print(bs.Iterative(arr, 3))
+	print(bs.Iterative(arr, 0))
+	print(bs.Iterative(arr, 5))
+	print(bs.Iterative(arr, 7))
+	print(bs.Iterative(arr, 8))
 
 
 

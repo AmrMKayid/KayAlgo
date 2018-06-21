9c26a2ae2c648812714dda6930d40015cfd26b15
Amr M. Kayid <amrkayid2027@gmail.com>
2018-06-21 07:56:42 +0200

    HackerRank Day 0

---
+++ HackerRank/day0-HelloWorld.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "os"
+)
+
+func main() {
+
+  reader := bufio.NewReader(os.Stdin)
+  text, _ := reader.ReadString('\n')
+  fmt.Println("Hello, World")
+  fmt.Println(text)
+  
+}

eb7b55ab43f48151d59d5de8757f7a78ae17dd75
Amr M. Kayid <amrkayid2027@gmail.com>
2018-06-24 12:55:40 +0200

    HackerRank Day 3 | conditional-statements

---
+++ day3-conditional-statements.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        System.out.println( ((n & 1) == 1 || (6 <= n && n <= 20)) ? "Weird" : "Not Weird");
+
+        scanner.close();
+    }
+}

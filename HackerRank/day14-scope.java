14f85e8127bf53bf1a420b9375a6b41c0524b1f1
Amr M. Kayid <amrkayid2027@gmail.com>
2018-07-14 11:58:45 +0200

    HackerRank Day 14 | scope

---
+++ HackerRank/day14-scope.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+
+  	// Add your code here
+
+    Difference(int[] elements) {
+        this.elements = elements;
+        this.maximumDifference = Integer.MIN_VALUE;
+    }
+
+    void computeDifference() {
+        Arrays.sort(elements);
+        maximumDifference = Math.abs(elements[elements.length - 1] - elements[0]);
+    }
+
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}

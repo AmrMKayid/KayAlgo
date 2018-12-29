986b0110ace75c767098ddadc3db86020db9211f
Amr M. Kayid <amrkayid2027@gmail.com>
2018-12-30 00:27:00 +0200

    [HackerRank] Finish 30 days of code

--- Internships19/2018.py
+++
@@ -1,1 +0,0 @@
-print('##### Interview Preparation Challenge #####')
---
+++ Internships19/G2018.py
@@ -0,0 +1,23 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(S, E):
+    # write your code in Python 3.6
+    S = sorted(S)
+    min_chairs = 1
+    party = dict(zip(S, E))
+    party = {person: party[person] for person in dict.fromkeys(sorted([person for person in party]))}
+    seated = [S[0]]
+    for person in S[1:]:
+        char = min(seated)
+        if person - party[char] < 0:
+            seated.append(person)
+            min_chairs += 1
+        else:
+            seated.append(person)
+            seated.pop(char)
+
+    return min_chairs
+
+
+print(solution([1, 2, 3, 4, 5], [2, 3, 4, 5, 6]))

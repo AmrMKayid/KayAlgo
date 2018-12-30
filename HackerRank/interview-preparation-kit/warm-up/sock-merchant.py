f65a8fb5cc3f68639981d6d09e97dd2e733766b8
Amr M. Kayid <amrkayid2027@gmail.com>
2018-12-31 01:03:21 +0200

    [HackerRank:IPK] warm up ✅

---
+++ HackerRank/interview-preparation-kit/warm-up/sock-merchant.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    colors = {}
+    for i in ar:
+        colors[i] = 0
+    for i in ar:
+        colors[i] += 1
+    print(colors)
+    pairs = 0
+    for color in colors:
+        pairs += colors[color] // 2
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

c1cb9c9de269f419ecadea2c90bda11b650bf26b
Amr M. Kayid <amrkayid2027@gmail.com>
2019-01-04 23:43:37 +0200

    [LeetCode] 03-longest-substring-without-repeating-characters

--- LeetCode/02-add-two-number.py
+++ LeetCode/02-add-two-number.py
@@ -11,12 +11,12 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        remainder = 0
+        carry = 0
         result = head = ListNode(0)
-        while l1 or l2 or remainder:
+        while l1 or l2 or carry:
             l1, v1 = [l1.next, l1.val] if l1 else [0, 0]
             l2, v2 = [l2.next, l2.val] if l2 else [0, 0]
-            remainder, num = divmod(v1 + v2 + remainder, 10)
+            carry, num = divmod(v1 + v2 + carry, 10)
             head.next = ListNode(num)
             head = head.next
         return result.next

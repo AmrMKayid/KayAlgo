4e39dbec1388fe1e6612d34980493e54003c5fd4
Amr M. Kayid <amrkayid2027@gmail.com>
2019-01-21 14:39:48 +0200

    [LeetCode] 268-missing-number.py

---
+++ LeetCode/268-missing-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        missing = len(nums)
+        for index, num in enumerate(nums):
+            missing ^= index ^ num
+        return missing

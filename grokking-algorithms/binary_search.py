cae5a1bcb2fccbbb61aa64851ff77567dc66e68d
AmrMKayid <amrmkayid@gmail.com>
2020-11-20 02:24:02 +0200

    Finish Chapter 1 ✅
    - Implement binary search algorithm
    - Refresh memory about Big O
    - Traveling Salesman Problem (TSP)

---
+++ grokking-algorithms/binary_search.py
@@ -0,0 +1,48 @@
+from typing import Any, List
+import random
+
+
+def binary_search(
+    *,
+    array: List[Any],
+    item: Any,
+) -> int:
+  """Binary search algorithm
+
+  Runtime: O(log(n))
+
+  Args:
+      array (List[Any]): Sorted array
+      item (Any): item to be searcher
+
+  Returns:
+      int: index of the item if exist
+  """
+
+  low = 0
+  high = len(array) - 1
+
+  while low <= high:
+    middle = int((low + high) / 2)
+    guess = array[middle]
+
+    if guess == item:
+      return middle
+
+    if guess < item:  ## Too low
+      low = middle + 1
+    else:  ## Too high
+      high = middle - 1
+
+  return None
+
+
+range_ = int(10e9)
+array = range(range_)
+item = random.randint(-range_, range_)
+print(f"Array: {array}\n Item: {item}")
+
+print(binary_search(
+    array=array,
+    item=item,
+))
